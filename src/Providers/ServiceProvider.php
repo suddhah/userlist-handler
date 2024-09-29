522eb344f9e3310190e5944df145525ae5953daf
Comadansu <comadansu@gmail.com>
2024-09-29 18:33:36 +1000

    feat: update service provider

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -3,11 +3,15 @@
 namespace Suddhah\UserListHandler\Providers;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Suddhah\UserListHandler\Factories\ParserFactory;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function boot(): void
     {
+        if (file_exists($file = __DIR__.'/../helpers.php')) {
+            require $file;
+        }
     }
 
     public function register(): void
